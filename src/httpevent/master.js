5a635c5a2d94a1875a0b96f8e349ee72329c949a
zhangjun11 <zhangjun11@kuaishou.com>
2022-01-20 14:33:13 +0800

    feat: 代码格式化

--- src/httpevent/master.js
+++ src/httpevent/master.js
@@ -1,26 +1,27 @@
-const fs = require('fs');
-const child_process = require('child_process');
-const koa = require('koa');
+const fs = require("fs");
+const child_process = require("child_process");
+const koa = require("koa");
 
 const app = new koa();
-app.use(async ctx => ctx.response.body = await run(ctx.request.path));
+app.use(async (ctx) => (ctx.response.body = await run(ctx.request.path)));
 app.listen(3000);
 
 async function run(path) {
-    return new Promise((resolve, reject) => {
-        
-        const child = child_process.fork('./child.js');
+  return new Promise((resolve, reject) => {
+    const child = child_process.fork("./child.js");
 
-        child.on('message', resolve);
+    child.on("message", resolve);
 
-        try {
-            const fn = fs.readFileSync(`./${path}.js`, { encoding: 'utf8' });
-            child.send({ action: 'run', fn });
-        } catch (error) {
-            if(error.code === 'ENOENT') {
-                return resolve('not fond function');
-            }
-            reject(error.toString());
-        }
-    });
+    console.log("test CR");
+
+    try {
+      const fn = fs.readFileSync(`./${path}.js`, { encoding: "utf8" });
+      child.send({ action: "run", fn });
+    } catch (error) {
+      if (error.code === "ENOENT") {
+        return resolve("not fond function");
+      }
+      reject(error.toString());
+    }
+  });
 }
