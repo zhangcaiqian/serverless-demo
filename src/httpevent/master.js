ee2e453cf5c30edec77162ba6ec5817b9830af6d
zhangjun11 <zhangjun11@kuaishou.com>
2022-04-14 14:43:00 +0800

    test: some error

--- src/httpevent/master.js
+++ src/httpevent/master.js
@@ -5,7 +5,7 @@ const koa = require("koa");
 const app = new koa();
 app.use(async (ctx) => (ctx.response.body = await run(ctx.request.path)));
 app.listen(3000);
-
+const a
 async function run(path) {
   return new Promise((resolve, reject) => {
     const child = child_process.fork("./child.js");
